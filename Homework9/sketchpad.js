b8daa1561cec720f9b9f067b5e57399291591274
PhantomCKA28 <CaitlynKAltemus@outlook.com>
2022-03-24 14:43:24 -0600

    Homework self portrait

---
+++ Homework9/sketchpad.js
@@ -0,0 +1,56 @@
+function setup()
+{
+    createCanvas(600, 800);
+}
+
+function draw()
+{
+   background(0,200,490);
+   fill(500,200,0);
+   circle(300,150,185);
+   fill(0,0,0);
+   circle(300,50,90);
+   fill(550,0,60);
+   ellipse(300,200,50,60);
+   fill(100,35,90);
+   square(235,245,140);
+   fill(0,90,130);
+   square(220,390,70);
+   fill(0,95,135);
+   square(320,390,70);
+   fill(25,100,400);
+   circle(265,130,30);
+   fill(25,100,400);
+   circle(340,130,30);
+   line(300,20,205,55);
+   line(400,35,305,60);
+   fill(150,30,100);
+   square(380,270,40);
+   fill(150,30,100);
+   square(190,270,40);
+   fill(190,50,290);
+   ellipse(90,600,100);
+   fill(0,500,150);
+   rect(90,180,50,90);
+   fill(70,90,180);
+   circle(590,790,110);
+   fill(800,15,90);
+   circle(100,490,80);
+   fill(728,809,50);
+   ellipse(299,680,200);
+   fill(380,166,70);
+   rect(470,150,90,155);
+   fill(189,616,07);
+   triangle(30,75,58,20,86,75);
+   textSize(35);
+   text('Caitlyn A', 400, 520);
+   fill(0,0,0);
+   textSize(45);
+   text('How I see myself', 80, 690);
+
+
+
+
+
+
+}
